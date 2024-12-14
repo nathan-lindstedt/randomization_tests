bec91cb1cbb7784d457fcb9fde092283f646e7f1
nathan-lindstedt <iconseq@gmail.com>
2024-12-14 07:21:47 -0700

    Added ter Braak (1992) method of permuated p-values and refactored permutation_test_regression()

--- randomization_tests.py
+++ randomization_tests.py
@@ -19,26 +19,25 @@ X = real_estate_valuation.data.features
 y = real_estate_valuation.data.targets 
 
 #%%
-def permutation_test_regression(
+# Define the permutation regression functions
+def calculate_p_values(
     X: pd.DataFrame, 
-    y: pd.DataFrame,  
-    n_permutations: int = 100_000,
+    y: pd.DataFrame,
+    permuted_coefs: List,
+    model_coefs: List,
     precision: int = 3,
     p_value_threshold_one: float = 0.05, 
     p_value_threshold_two: float = 0.01
-) -> Tuple[List[float], List[str], List[str], float, float]:
+) -> Tuple[List[str], List[str]]:
     """
-    Perform a permutation test for a multiple linear regression model to assess the significance of model 
-    coefficients using the Manly (1997) method.
-
+    Calculate p-values for a given model using both permutation and classical methods.
+    
     Parameters
     ----------
     X : array-like of shape (n_samples, n_features)
         The input data to fit the model.
     y : array-like of shape (n_samples,)
         The target values.
-    n_permutations : int, optional (default=100_000)
-        The number of permutations to perform.
     precision : int, optional (default=3)
         The number of decimal places to round the p-values.
     p_value_threshold_one : float, optional (default=0.05)
@@ -54,40 +53,24 @@ def permutation_test_regression(
         The empirical p-values for each coefficient, formatted with significance stars if below the thresholds.
     classic_p_values : list of str
         The asymptotic p-values for each coefficient, formatted with significance stars if below the thresholds.
-
-    References
-    ----------
-    Manly, Bryan F. J. Randomization, Bootstrap, and Monte Carlo Methods in Biology, 2nd ed. Texts in Statistical 
-    Science Series. Chapman & Hall, London. (1997).
-    
-    Hardin, Johanna, Lauren Quesada, Julie Ye, and Nicholas J. Horton. "The Exchangeability Assumption for 
-    Purmutation Tests of Multiple Regression Models: Implications for Statistics and Data Science Educators." 
-    (2024) [Online]. Available: https://arxiv.org/pdf/2406.07756.
     """
-    permuted_coefs: List = []
     permuted_p_values: List = []
     classic_p_values: List = []
 
-    model = LinearRegression().fit(X, y)
-    model_coefs = model.coef_.flatten().tolist()
-
-    for _ in range(n_permutations):
-        model.fit(X, np.random.permutation(y))
-        permuted_coefs.append(model.coef_.flatten().tolist())
+    model = sm.OLS(y, sm.add_constant(X)).fit()
 
     for i in range(len(model_coefs)):
-        p_value = (np.abs(np.array(permuted_coefs)[:, i]) >= np.abs(np.array(model_coefs)[i])).mean()
+        p_value = (np.abs(np.array(permuted_coefs)[:, i]) >= np.abs(np.array(model_coefs)[i])).mean()        
         if p_value_threshold_two <= p_value < p_value_threshold_one:
             p_value_str = str(np.round(p_value, precision)) + ' (*)'
         elif p_value < p_value_threshold_two:
             p_value_str = str(np.round(p_value, precision)) + ' (**)'
         else:
             p_value_str = str(np.round(p_value, precision)) + ' (ns)'
+        
         permuted_p_values.append(p_value_str)
-
-    sm_model = sm.OLS(y, sm.add_constant(X)).fit()
-
-    for p_value in sm_model.pvalues[1:]:
+    
+    for p_value in model.pvalues[1:]:
         if p_value_threshold_two <= p_value < p_value_threshold_one:
             p_value_str = str(np.round(p_value, precision)) + ' (*)'
         elif p_value < p_value_threshold_two:
@@ -96,10 +79,87 @@ def permutation_test_regression(
             p_value_str = str(np.round(p_value, precision)) + ' (ns)'
         classic_p_values.append(p_value_str)
 
+    return permuted_p_values, classic_p_values
+
+def permutation_test_regression(
+    X: pd.DataFrame, 
+    y: pd.DataFrame,  
+    n_permutations: int = 100_000,
+    precision: int = 3,
+    p_value_threshold_one: float = 0.05, 
+    p_value_threshold_two: float = 0.01,
+    method: str = 'manly'
+) -> Tuple[List[float], List[str], List[str], float, float]:
+    """
+    Perform a permutation test for a multiple linear regression model to assess the significance of model 
+    coefficients using the ter Braak (1992) or Manly (1997) methods.
+
+    Parameters
+    ----------
+    X : array-like of shape (n_samples, n_features)
+        The input data to fit the model.
+    y : array-like of shape (n_samples,)
+        The target values.
+    n_permutations : int, optional (default=100_000)
+        The number of permutations to perform.
+    precision : int, optional (default=3)
+        The number of decimal places to round the p-values.
+    p_value_threshold_one : float, optional (default=0.05)
+        The threshold for the first level of significance.
+    p_value_threshold_two : float, optional (default=0.01)
+        The threshold for the second level of significance.
+    method : str, optional (default='manly')
+
+    Methods
+    ----------
+    'terbraak' : Permutation test using the method described by ter Braak (1992).
+    'manly' : Permutation test using the method described by Manly (1997).
+
+    Returns
+    ----------
+    model_coefs : list of float
+        The original coefficients of the fitted model.
+    permuted_p_values : list of str
+        The empirical p-values for each coefficient, formatted with significance stars if below the thresholds.
+    classic_p_values : list of str
+        The asymptotic p-values for each coefficient, formatted with significance stars if below the thresholds.
+
+    References
+    ----------
+    ter Braak, Cajo J. F. "Permutation versus bootstrap significance tests in multiple regression and ANOVA."
+    In Handbook of Statistics, Vol. 9. Elsevier, Amsterdam. (1992).
+
+    Manly, Bryan F. J. Randomization, Bootstrap, and Monte Carlo Methods in Biology, 2nd ed. Texts in Statistical 
+    Science Series. Chapman & Hall, London. (1997).
+    
+    Hardin, Johanna, Lauren Quesada, Julie Ye, and Nicholas J. Horton. "The Exchangeability Assumption for 
+    Purmutation Tests of Multiple Regression Models: Implications for Statistics and Data Science Educators." 
+    (2024) [Online]. Available: https://arxiv.org/pdf/2406.07756.
+    """
+    permuted_coefs: List = []
+
+    model = LinearRegression().fit(X, y)
+    model_coefs = np.ravel(model.coef_).tolist()
+    model_preds = model.predict(X)
+    model_resids = y - model_preds
+
+    if method == 'manly':
+        for _ in range(n_permutations):
+            model.fit(X, np.random.permutation(y))
+            permuted_coefs.append(np.ravel(model.coef_).tolist())
+    elif method == 'terbraak':
+        for _ in range(n_permutations):
+            model.fit(X, np.random.permutation(model_preds) + np.random.permutation(model_resids))
+            permuted_coefs.append(np.ravel(model.coef_).tolist())
+    else:
+        raise ValueError("Invalid method. Please select 'manly' or 'terbraak'.")
+
+    permuted_p_values, classic_p_values = calculate_p_values(X, y, permuted_coefs, model_coefs, precision, p_value_threshold_one, p_value_threshold_two)
+    
     return model_coefs, permuted_p_values, classic_p_values, p_value_threshold_one, p_value_threshold_two
 
 #%%
-# Perform permutation test
+# Perform permutation test by the Manly (1997) method
 (
     coefs, 
     permuted_p_values, 
@@ -109,8 +169,30 @@ def permutation_test_regression(
 ) = permutation_test_regression(X, y)
 
 #%%
-# Print the results
-print("Regression Model Coefficients and p-Values\n")
+# Print the results obtained by the Manly (1997) method
+print("Regression Model Coefficients and p-Values obtained by the Manly (1997) method\n")
+print(f"Target: {y.columns.tolist()}")
+print(f"Features: {X.columns.tolist()}\n")
+print(f"Coefficients: {coefs}\n")
+print(f"Empirical p-Values: {permuted_p_values}")
+print(f"Asymptotic p-Values: {classic_p_values}")
+print(f"\n(*) p-value < {p_value_threshold_one}")
+print(f"(**) p-value < {p_value_threshold_two}")
+print(f"(ns) p-value >= {p_value_threshold_one}\n")
+
+#%%
+# Perform permutation test by the ter Braak (1992) method
+(
+    coefs, 
+    permuted_p_values, 
+    classic_p_values, 
+    p_value_threshold_one, 
+    p_value_threshold_two
+) = permutation_test_regression(X, y, method='terbraak')
+
+#%%
+# Print the results obtained by the ter Braak (1992) method
+print("Regression Model Coefficients and p-Values obtained by the ter Braak (1992) method\n")
 print(f"Target: {y.columns.tolist()}")
 print(f"Features: {X.columns.tolist()}\n")
 print(f"Coefficients: {coefs}\n")
